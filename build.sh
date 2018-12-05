7acb0e02110af65682d42a8cf5211d80eee48b43
brunneis <dev@brunneis.com>
2018-12-05 14:46:49 +0100

    Add env.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-KAFKA_VERSION=$(cat KAFKA_VERSION)
-KAFKA_SUBVERSION=$(cat KAFKA_SUBVERSION)
+source env.sh
 docker build \
 --build-arg KAFKA_VERSION=$KAFKA_VERSION \
 --build-arg KAFKA_SUBVERSION=$KAFKA_SUBVERSION -t catenae/kafka .
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-KAFKA_VERSION=$(cat KAFKA_VERSION)
+source env.sh
 docker push catenae/kafka
 docker push catenae/kafka:$KAFKA_VERSION
